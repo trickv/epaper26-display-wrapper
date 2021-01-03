b6b3726daa4330b4c60425f15cf16dc864ff9f18
Patrick van Staveren <trick@vanstaveren.us>
2021-01-03 11:56:00 -0600

    add argparse --test flag

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+import argparse
+
 from PIL import Image
 from PIL import ImageDraw
 
-mode = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", action="store_true", help="Test mode which uses xv to display the generated image locally")
+args = parser.parse_args()
+test_mode=args.test
 
 # NB: mode P means 8 bit indexed; i might later just make two grayscale images one for each color instead of doing an indexed color version and splitting it out.
 p = Image.new(mode='P', size=(296, 152), color=(255,255,255))
@@ -14,7 +20,7 @@ draw.line((0, p.size[1], p.size[0], 0), fill=128)
 del draw
 
 
-if mode == "test":
+if test_mode:
     p.show()
 else:
     p.save("foo.bmp")
