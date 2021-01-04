ab1d80d6b4ab7d017643531ea0103d181e343dea
Patrick van Staveren <trick@vanstaveren.us>
2021-01-04 14:31:34 +0000

    split into two monochrome images, copy in place and execute epd demo. it should work.

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
+import os
+
 from PIL import Image
 from PIL import ImageDraw
 
 mode = False
 
 # NB: mode P means 8 bit indexed; i might later just make two grayscale images one for each color instead of doing an indexed color version and splitting it out.
-p = Image.new(mode='P', size=(296, 152), color=(255,255,255))
+black = Image.new(mode='1', size=(296, 152), color=(255))
 
-draw = ImageDraw.Draw(p)
-draw.line((0, 0) + p.size, fill=128)
-draw.line((0, p.size[1], p.size[0], 0), fill=128)
+draw = ImageDraw.Draw(black)
+draw.line((0, 0) + black.size, fill=128)
+draw.line((0, black.size[1], black.size[0], 0), fill=128)
+del draw
+
+red = Image.new(mode='1', size=(296, 152), color=(255))
+
+draw = ImageDraw.Draw(black)
+draw.line((0, 0) + red.size, fill=128)
+draw.line((0, red.size[1], red.size[0], 0), fill=128)
 del draw
 
 
 if mode == "test":
-    p.show()
+    black.show()
+    red.show()
 else:
-    p.save("foo.bmp")
+    black.save("black.bmp")
+    red.save("red.bmp")
+    os.system("cp black.bmp buydisplay-epaper26-example-adaptation/wiringpi/pic/cw-1rb1.bmp")
+    os.system("cp red.bmp buydisplay-epaper26-example-adaptation/wiringpi/pic/cw-1rb2.bmp")
+    os.system("cd buydisplay-epaper26-example-adaptation/wiringpi/ && ./epd")
