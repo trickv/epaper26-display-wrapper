6be363dd13c16fcc70e60648a1aac2fb2f77a6b9
Patrick van Staveren <trick@vanstaveren.us>
2021-02-24 16:21:49 -0600

    remove distracting DC Volts and aspirational today/yesterday placeholders

--- main.py
+++ main.py
@@ -63,12 +63,12 @@ solar_today_value = solar_yesterday_value = "-1"
 draw_red.text((2, 2), "Solar:", fill=0, font=font30)
 draw_red.text((10, 35), "Now:", fill=0, font=font15)
 draw_black.text((80, 35), solar_now_value, fill=0, font=font15)
-draw_red.text((10, 55), "DC Volts:", fill=0, font=font15)
-draw_black.text((80, 55), dc_volts, fill=0, font=font15)
-draw_red.text((10, 75), "Today:", fill=0, font=font15)
-draw_black.text((80, 75), solar_today_value, fill=0, font=font15)
-draw_red.text((10, 95), "Yesterday:", fill=0, font=font15)
-draw_black.text((80, 95), solar_yesterday_value, fill=0, font=font15)
+#draw_red.text((10, 55), "DC Volts:", fill=0, font=font15)
+#draw_black.text((80, 55), dc_volts, fill=0, font=font15)
+#draw_red.text((10, 75), "Today:", fill=0, font=font15)
+#draw_black.text((80, 75), solar_today_value, fill=0, font=font15)
+#draw_red.text((10, 95), "Yesterday:", fill=0, font=font15)
+#draw_black.text((80, 95), solar_yesterday_value, fill=0, font=font15)
 draw_red.text((50, 190), now_chicago, fill=0, font=font15)
 del draw_red
 del draw_black
