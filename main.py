51107e58dffa6fdfee5535890169a079fcec604e
Patrick van Staveren <trick@vanstaveren.us>
2021-04-04 17:05:58 -0500

    hack in today thus far value

--- main.py
+++ main.py
@@ -50,13 +50,20 @@ conn.close
 now = json.loads(response)
 solar_now_value = "{0}".format(int(float(now['data']['result'][0]['value'][1])))
 
+conn = http.client.HTTPSConnection("vanstaveren.us")
+conn.request("GET", "/~trick/epaper/solaredge-today.cgi")
+response = conn.getresponse().read()
+conn.close
+now = json.loads(response)
+solaredge_today_value = "{0}".format(int(float(now['state'])))
+
 
 solar_today_value = solar_yesterday_value = "-1"
 draw_red.text((2, 2), "Solar:", fill=0, font=font30)
 #draw_red.text((10, 35), "Now:", fill=0, font=font15)
 draw_black.text((0, 45), solar_now_value, fill=0, font=font30)
 #draw_red.text((10, 55), "DC Volts:", fill=0, font=font15)
-#draw_black.text((80, 55), dc_volts, fill=0, font=font15)
+draw_black.text((0, 85), solaredge_today_value, fill=0, font=font30)
 #draw_red.text((10, 75), "Today:", fill=0, font=font15)
 #draw_black.text((80, 75), solar_today_value, fill=0, font=font15)
 #draw_red.text((10, 95), "Yesterday:", fill=0, font=font15)
